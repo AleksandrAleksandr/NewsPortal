9bfe940af9f648a687fdbb439c9e3d539e9e0ed3
Alexandr <sanek.s.98@mail.ru>
2021-08-20 18:00:46 +0300

    move unwrapping result class to view model

--- app/src/main/java/com/example/newsportal/app/topnews/CategoryFragment.kt
+++ app/src/main/java/com/example/newsportal/app/topnews/CategoryFragment.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.newsportal.utils.NewsAdapter
 import com.example.newsportal.databinding.FragmentCategoryBinding
 import com.example.newsportal.domain.model.Article
-import com.example.newsportal.utils.ResultWrapper
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class CategoryFragment : Fragment() {
@@ -34,15 +35,16 @@ class CategoryFragment : Fragment() {
         val adapter = NewsAdapter()
         binding.rvNews.adapter = adapter
 
-        viewModel.data.observe(viewLifecycleOwner, { status ->
-            when(status){
-                is ResultWrapper.Success -> adapter.setData(filteredNews(status.data)).also { setLoading(false) }
-                is ResultWrapper.Error -> {
-                    binding.tv.text = status.msg
-                    setLoading(false)
-                }
-                ResultWrapper.Loading -> setLoading(true)
-            }
+        viewModel.data.observe(viewLifecycleOwner, { data ->
+            adapter.setData(filteredNews(data))
+        })
+
+        viewModel.isLoading.observe(viewLifecycleOwner, {
+            setLoading(it)
+        })
+
+        viewModel.error.observe(viewLifecycleOwner, { message ->
+            showError(message)
         })
 
     }
@@ -50,8 +52,12 @@ class CategoryFragment : Fragment() {
     private fun filteredNews(news: List<Article>) =
         news.filter { it.category == category }
 
-    private fun setLoading(loading: Boolean){
-        binding.progressBar.visibility = if (loading) View.VISIBLE else View.GONE
+    private fun setLoading(loading: Boolean) {
+        binding.progressBar.isVisible = loading
+    }
+
+    private fun showError(msg: String) {
+        Toast.makeText(requireContext(), msg, Toast.LENGTH_LONG).show()
     }
 
     companion object {
