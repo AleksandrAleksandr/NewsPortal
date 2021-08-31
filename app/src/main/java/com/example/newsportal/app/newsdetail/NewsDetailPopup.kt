b48cb66554496da93d50a177a9340c6c6a4b7fb9
Alexandr <sanek.s.98@mail.ru>
2021-08-31 18:18:03 +0300

    comments applied

--- app/src/main/java/com/example/newsportal/app/newsdetail/NewsDetailPopup.kt
+++ app/src/main/java/com/example/newsportal/app/newsdetail/NewsDetailPopup.kt
@@ -1,12 +1,10 @@
 package com.example.newsportal.app.newsdetail
 
 import android.content.Context
-import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.PopupWindow
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import com.example.newsportal.R
 import com.example.newsportal.databinding.PopupNewsDetailBinding
@@ -23,20 +21,17 @@ class NewsDetailPopup(
 
     init {
         binding.apply {
-            topAppBar.title = ""
             topAppBar.setNavigationOnClickListener { listener.onBackButtonClicked() }
             topAppBar.setOnMenuItemClickListener { menuItem ->
                 if (menuItem.itemId == R.id.browser) {
-                    menuItem.isEnabled = false
                     listener.onBrowserButtonClicked()
                     true
                 } else false
             }
             tvTitle.text = article.title
             tvDescription.text = article.description
-            //tvAuthor.text = Resources.getSystem().getString(R.string.author_template, article.author)
-            tvAuthor.text = "Author: " + article.author
-            tvPublishedAt.text = article.publishedAt.take(10).toFormat()
+            tvAuthor.text = binding.root.context.getString(R.string.author_template)
+            tvPublishedAt.text = article.publishedAt.take(DATE_LENGTH).toFormat()
             Glide.with(articleImage).load(article.urlToImage).error(R.drawable.ic_newspaper2)
                 .centerCrop().into(articleImage)
         }
@@ -44,6 +39,7 @@ class NewsDetailPopup(
     }
 
     companion object {
+        private const val DATE_LENGTH = 10
 
         fun newInstance(
             context: Context,
@@ -55,7 +51,7 @@ class NewsDetailPopup(
                 PopupNewsDetailBinding.inflate(LayoutInflater.from(context), container, false)
             return NewsDetailPopup(
                 LinearLayout.LayoutParams.MATCH_PARENT,
-                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
                 binding,
                 listener,
                 article
